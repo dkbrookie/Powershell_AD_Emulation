3b1cd651c7ad97b1053e60a03735d133e0add345
dkbrookie <42004970+dkbrookie@users.noreply.github.com>
2021-09-24 16:23:37 -0500

    Added output to work with the Automate monitor format

--- Interactive_Logon/Do_Not_Display_Last_Username.ps1
+++ Interactive_Logon/Do_Not_Display_Last_Username.ps1
@@ -5,13 +5,16 @@ Try {
     If ((Get-ItemProperty -Path $regPath -Name dontdisplaylastusername) -ne 1) {
         $output += "[dontdisplaylastusername] at $regPath was not set to 1, setting to 1 now..."
         Set-ItemProperty -Path $regPath -Name dontdisplaylastusername -Value 1
-        $output += "Successfully set [dontdisplaylastusername] at $regPath to 1!"
+        $output += "!SUCCESS: Successfully set [dontdisplaylastusername] at $regPath to 1!"
     } Else {
-        $output += "Verified [dontdisplaylastusername] at $regPath is already set to 1!"
+        $output += "!SUCCESS: Verified [dontdisplaylastusername] at $regPath is already set to 1!"
     }
+    $output = $output -join "`n"
     Return $output
+    Break
 } Catch {
-    $output += "There was a problem when attempting to set [dontdisplaylastusername] at $regPath to the value of 1. Full error output: $Error"
+    $output += "!FAILED: There was a problem when attempting to set [dontdisplaylastusername] at $regPath to the value of 1. Full error output: $Error"
+    $output = $output -join "`n"
     Return $output
     Break
 }
